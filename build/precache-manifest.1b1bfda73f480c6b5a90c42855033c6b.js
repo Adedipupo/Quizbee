423e027937b62c4ca30e8280c176eb3c2b9829b8
Adedipupo <bayonleolaluwa@gmail.com>
2020-04-07 01:11:23 +0000

    new files

---
+++ build/precache-manifest.1b1bfda73f480c6b5a90c42855033c6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ecacad75f6cba614bd358b5f242a437",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c16ae41cb7c75986c43a",
+    "url": "/static/css/main.a428579f.chunk.css"
+  },
+  {
+    "revision": "9eb703db35f2c3e738c5",
+    "url": "/static/js/2.dc09447c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.dc09447c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c16ae41cb7c75986c43a",
+    "url": "/static/js/main.d0ce97f4.chunk.js"
+  },
+  {
+    "revision": "293f968637a1c320dba5",
+    "url": "/static/js/runtime-main.b085c3d5.js"
+  }
+]);
